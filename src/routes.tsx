4b9b456f217aec4d794150b4df23e55469af9427
applicationjob <usaferreiratiago@gmail.com>
2026-06-29 09:19:24 -0300

    senoengenharia

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+import Projetos from "./pages/Projetos";
+import Servicos from "./pages/Serviços";
 
 export default function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/contato" element={<Contato />} />
+      <Route path="/projetos" element={<Projetos />} />
+      <Route path="/servicos" element={<Servicos />} />
     </Routes>
   );
 }
