b3059963791a2bdd6f3e8bb73121df55dd50dd92
Tiago Ferreira <usaferreiratiago@gmail.com>
2026-06-29 15:00:54 -0300

    senoengenharia

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // 1. IMPORTAR O PLUGIN DO TAILWIND V4
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(), // 2. ATIVAR O PLUGIN DO TAILWIND ANTES DO REACT
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
